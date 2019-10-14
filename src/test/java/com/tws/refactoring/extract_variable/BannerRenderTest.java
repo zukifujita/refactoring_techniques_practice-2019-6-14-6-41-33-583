8f0cb7d0dcdf23d08dd4c7405347461664a98a2b
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-14 20:58:14 +0800

    Added BannerRenderTest.java

--- src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
@@ -5,9 +5,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BannerRenderTest {
-
     @Test
-    public void should_calculate() {
+    public void should_render_banner() {
+        BannerRender banner = new BannerRender();
 
+        String platform = "MAC";
+        String browser = "IE";
+
+        String result = banner.renderBanner(platform, browser);
+
+        assertEquals("IE on Mac?", result);
     }
 }
