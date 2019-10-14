1fd1a49a160a35a23567c4b198c5d828884664b2
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-14 20:54:22 +0800

    Refactored OwingPrinter.java

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -1,40 +1,26 @@
 package com.tws.refactoring.extract_method;
 
-import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Vector;
 
 public class OwingPrinter {
-    void printOwing(String name, List<Order> orders) {
-        Iterator<Order> elements = orders.iterator();
-        double outstanding = 0.0;
+    public void printOwing(String name, List<Order> orders) {
+        StringBuilder receipt = new StringBuilder();
+        Double total = getTotal(orders);
+        receipt.append("*****************************\n")
+                .append("****** Customer totals ******\n")
+                .append("*****************************\n")
+                .append("name: " + name + "\n")
+                .append("amount: " + total);
+        System.out.print(receipt.toString());
+    }
 
-        // print banner
-        System.out.println ("*****************************");
-        System.out.println ("****** Customer totals ******");
-        System.out.println ("*****************************");
-
-        // print owings
-        while (elements.hasNext()) {
-            Order each = (Order) elements.next();
-            outstanding += each.getAmount();
+    private double getTotal(List<Order> orders) {
+        Double totalAmount = 0.0;
+        for (Order order : orders) {
+            totalAmount += order.getAmount();
         }
-
-        // print details
-        System.out.println("name: " + name);
-        System.out.println("amount: " + outstanding);
-    }
-}
-
-class Order {
-    private final double amount;
-
-    public Order(double amount) {
-        this.amount = amount;
+        return totalAmount;
     }
 
-    public double getAmount() {
-        return amount;
-    }
 }
