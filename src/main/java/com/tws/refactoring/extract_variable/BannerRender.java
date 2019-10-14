7733de3dc5296bb311d2d547e4029fb958c0c151
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-14 20:55:35 +0800

    Refactored BannerRender.java

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,10 +2,14 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
-            return "IE on Mac?";
-        }
+        if (isBrowserOnPlatform(platform, browser)) return "IE on Mac?";
         return "banner";
+    }
+
+    private boolean isBrowserOnPlatform(String platform, String browser) {
+        if ((platform.toUpperCase().indexOf("MAC") > -1) && (browser.toUpperCase().indexOf("IE") > -1)) {
+            return true;
+        }
+        return false;
     }
 }
