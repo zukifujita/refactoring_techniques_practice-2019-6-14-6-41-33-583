a8236fa531e475daf1c71a554691d11fbceb34b8
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-14 20:56:06 +0800

    Refactored PriceCalculator.java

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,10 +1,17 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
+    private double basePrice = 0d;
+    private double discount = 0d;
+    private double shippingCost = 0d;
+    private double allExpenses = 0d;
+
     double getPrice(int quantity, int itemPrice) {
         // Price consists of: base price - discount + shipping cost
-        return quantity * itemPrice -
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+        basePrice = quantity * itemPrice;
+        discount = Math.max(0, quantity - 500) * itemPrice * 0.05;
+        shippingCost = Math.min(quantity * itemPrice * 0.1, 100.0);
+        allExpenses = basePrice - discount + shippingCost;
+        return allExpenses;
     }
 }
