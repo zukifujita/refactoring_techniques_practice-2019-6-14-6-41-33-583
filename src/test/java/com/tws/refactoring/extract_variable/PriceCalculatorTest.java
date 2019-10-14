75b8f8ca34f9f4fa89a66b2264f8b2bcc5203b2d
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-14 20:58:37 +0800

    Added PriceCalculatorTest.java

--- src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
@@ -3,13 +3,16 @@ package com.tws.refactoring.extract_variable;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class PriceCalculatorTest {
     @Test
-    public void should_calculate() {
+    public void should_calculate_total_price() {
         PriceCalculator calculator = new PriceCalculator();
 
+        double result = calculator.getPrice(2, 5);
 
+        assertThat(11.0, is(result));
     }
 }
